192f4c118ac09871b54fb1d5fcc9ed5973f90c6c
Filip Hodun <filiphodun@gmail.com>
2021-03-06 16:32:48 +0100

    readme update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="leaguelosestreakmeter-fhodun",
+    name="leaguelosestreakmeter",
     version="0.0.1",
     author='Filip Hodun',
     author_email='filiphodun@gmail.com',
